664a13b89bf0b9ae84752fcbf04c9479f085d732
root <root@localhost.localdomain>
2021-05-02 15:49:42 -0500

    Login Creado

---
+++ facturacion/conexion.php
@@ -0,0 +1,14 @@
+<?php 
+	
+	$host = 'localhost';
+	$user = 'DarkLight';
+	$password = 'Juanito9119';
+	$db = 'facturacion';
+
+	$conection = @mysqli_connect($host,$user,$password,$db);
+
+	if(!$conection){
+		echo "Error en la conexión";
+	}
+
+?>
