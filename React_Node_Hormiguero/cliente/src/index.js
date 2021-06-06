3366f57f96cf96a3230f2dfbdc4948c1b2a23bf6
root <root@localhost.localdomain>
2021-06-06 13:43:22 -0500

    Fusionando Cambios

--- React_Node_Hormiguero/cliente/src/index.js
+++ React_Node_Hormiguero/cliente/src/index.js
@@ -4,21 +4,42 @@ import './index.css';
 
 
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
 
-function App() {
-  return (
-    <div>
-      <Welcome name="Sara" /> 
-      <Welcome name="Cahal" />      
-      <Welcome name="Edite" />    
-    </div>
-  );
-}
+  
+
+const usuario ={
+  primernombre: 'Ingeniero Exitoso',
+  segundonombre: 'Maria Celeste Rodriguez Barragan'
+};
+
+
+  function formatonombre(usuario){
+    return usuario.primernombre + ' ' + usuario.segundonombre;
+  }
+
+  function obtener(usuario){
+
+      if(usuario){
+        return <h3>Hola Cele, {formatonombre(usuario)}!!</h3>;
+      }
+      return <h1>hola , Señor Padre.</h1>;
+  }
+
+
+  const element = ( <h1> Hola, {obtener(usuario)}!!!! ___ DarkLight</h1>);
+
+
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+
+  //** element, llamando constantes desde las variables element, ingeniero, */  
+    
+    element,
+    
+    document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
